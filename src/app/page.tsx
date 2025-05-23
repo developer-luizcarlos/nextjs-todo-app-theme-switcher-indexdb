6559b82d166303ec524aae898a4aaf26cebc5632
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-23 14:02:13 -0300

    feat: create header element (styled)
    The background image of this element changes
    based on the current theme.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeContext/ThemeProvider";
+import { useContext } from "react";
+
 const Home: React.FC = () => {
-  return <></>;
+  const { theme } = useContext(ThemeContext)!;
+
+  return (
+    <>
+      <header
+        className={`h-96 ${
+          theme === "light"
+            ? "bg-[url('/images/bg-mobile-light.jpg')] md:bg-[url('/images/bg-desktop-light.jpg')]"
+            : "bg-[url('/images/bg-mobile-dark.jpg')] md:bg-[url('/images/bg-desktop-dark.jpg')]"
+        } bg-center bg-cover flex items-center justify-center`}
+      ></header>
+    </>
+  );
 };
 
 export default Home;
