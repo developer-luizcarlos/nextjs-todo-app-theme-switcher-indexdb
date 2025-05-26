76566262d7936a0bebe78401ec0e4ffb07c1474c
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-26 14:57:03 -0300

    feat: create app title and icon to swap theme (without logic)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ThemeContext } from "@/context/ThemeContext/ThemeProvider";
+import Image from "next/image";
 import { useContext } from "react";
 
 const Home: React.FC = () => {
@@ -15,6 +16,26 @@ const Home: React.FC = () => {
             : "bg-[url('/images/bg-mobile-dark.jpg')] md:bg-[url('/images/bg-desktop-dark.jpg')]"
         } bg-center bg-cover flex items-center justify-center`}
       ></header>
+      <main
+        className={`min-h-dvh ${
+          theme === "light" ? "bg-very-light-grayish-blue" : "bg-very-dark-blue"
+        }`}
+      ></main>
+      <article className="absolute top-36 left-1/2 -translate-1/2 w-1/2 max-w-[500px] min-w-[250px]">
+        <header className="flex items-baseline justify-between">
+          <h1 className="text-5xl uppercase font-extrabold text-very-light-gray">
+            todo
+          </h1>
+          <Image
+            src={`${
+              theme === "light" ? "images/icon-moon.svg" : "images/icon-sun.svg"
+            }`}
+            alt="Change theme"
+            height={30}
+            width={30}
+          />
+        </header>
+      </article>
     </>
   );
 };
